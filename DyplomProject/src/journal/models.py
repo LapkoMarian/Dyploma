21e28162839bb526ef6d47143d0b0385f2b7c225
Marian Lapko <lapkomarian2002@gmail.com>
2023-06-10 20:35:01 +0300

    add journal model

--- DyplomProject/src/journal/models.py
+++ DyplomProject/src/journal/models.py
@@ -1,3 +1,26 @@
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
+from classroom.models import ClassroomUsers
 
-# Create your models here.
+
+class Rating(models.IntegerChoices):
+    one = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+    ten = 10
+    eleven = 11
+    twelve = 12
+
+
+class Journal(models.Model):
+    rating = models.IntegerField(choices=Rating.choices)
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(default=timezone.now)
+    classroom_user_id = models.ForeignKey(ClassroomUsers, on_delete=models.CASCADE)
