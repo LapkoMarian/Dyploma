634f36b9aca8a8153232845967b63f2f2430c833
Marian Lapko <lapkomarian2002@gmail.com>
2023-06-11 19:29:17 +0300

    fix static

--- DyplomProject/src/core/settings.py
+++ DyplomProject/src/core/settings.py
@@ -113,8 +113,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR/'static',)
 
 
 LOGIN_URL = 'users:login'
